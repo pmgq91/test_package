6e9355d76c76e2037ba9631d503b13703228ea9a
pedromarvan <62215568+pmgq91@users.noreply.github.com>
2021-09-22 20:27:19 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(    
+    name='pmgq_test',
+    url='https://github.com/pmgq91/test_package',
+    author='Pedro Marvan',
+    author_email='pmgq@hotmail.com',
+    packages=['pmgq_test'],
+    # install_requires=['numpy'],
+    license='PMGQ',
+    description='An example of a python package from pre-existing code'
+)
